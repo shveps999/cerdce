29563db87c3eff8ecb87d41b8479f2c1a538f238
shveps999 <privetzdarovahappy@gmail.com>
2025-07-16 22:49:12 +0300

    Update main_keyboard.py

--- events_bot/bot/keyboards/main_keyboard.py
+++ events_bot/bot/keyboards/main_keyboard.py
@@ -5,7 +5,7 @@ from aiogram.types import InlineKeyboardMarkup
 def get_main_keyboard() -> InlineKeyboardMarkup:
     """Главная инлайн-клавиатура с основными действиями (выравнивание по левому краю)"""
     builder = InlineKeyboardBuilder()
-    builder.button(text="📮 Смотреть подборку актуальных событий", callback_data="feed")
+    builder.button(text="📮 Смотреть подборку", callback_data="feed")
     builder.button(text="✏️ Создать свое событие", callback_data="create_post")
     builder.button(text="🤍 Смотреть мои события", callback_data="my_posts")
     builder.button(text="🎓 Изменить университет", callback_data="change_city")
