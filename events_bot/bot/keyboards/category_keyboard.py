58f568d79ac363c1f42a21aed5ecbe2ba232541b
shveps999 <privetzdarovahappy@gmail.com>
2025-07-17 02:05:14 +0300

    Update category_keyboard.py

--- events_bot/bot/keyboards/category_keyboard.py
+++ events_bot/bot/keyboards/category_keyboard.py
@@ -32,7 +32,7 @@ def get_category_selection_keyboard(
     
     for category in categories:
         is_selected = category.id in selected_ids
-        text = f"{'❤️' if is_selected else '🤍'} {category.name}"
+        text = f"{category.name} {'❤️' if is_selected else '🤍'}"
         builder.button(text=text, callback_data=f"{prefix}{category.id}")
     builder.adjust(2)
     
