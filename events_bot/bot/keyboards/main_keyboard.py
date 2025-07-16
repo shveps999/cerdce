190bdb00738743a47ad8923e59f5547dab47d30f
shveps999 <privetzdarovahappy@gmail.com>
2025-07-16 21:37:10 +0300

    Update main_keyboard.py

--- events_bot/bot/keyboards/main_keyboard.py
+++ events_bot/bot/keyboards/main_keyboard.py
@@ -10,7 +10,7 @@ def get_main_keyboard() -> InlineKeyboardMarkup:
     builder.button(text="📊 Мои события", callback_data="my_posts")
     builder.button(text="🏙️ Изменить город", callback_data="change_city")
     builder.button(text="📂 Изменить интересы", callback_data="change_category")
-    builder.button(text="🔍 Модерация", callback_data="moderation")
+    builder.button(text="❤︎ Модерация", callback_data="moderation")
     builder.button(text="ℹ️ Помощь", callback_data="help")
     
     # Ключевое изменение - выравниваем все кнопки по левому краю
