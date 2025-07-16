a22183b59862b35d322b149fd87d71d16a2386e9
shveps999 <privetzdarovahappy@gmail.com>
2025-07-16 22:04:46 +0300

    Update main_keyboard.py

--- events_bot/bot/keyboards/main_keyboard.py
+++ events_bot/bot/keyboards/main_keyboard.py
@@ -6,10 +6,10 @@ def get_main_keyboard() -> InlineKeyboardMarkup:
     """Главная инлайн-клавиатура с основными действиями (выравнивание по левому краю)"""
     builder = InlineKeyboardBuilder()
     builder.button(text="▶︎ Смотреть подборку", callback_data="feed")
-    builder.button(text="Создать свое событие", callback_data="create_post")
-    builder.button(text="Мои события", callback_data="my_posts")
-    builder.button(text="Изменить город", callback_data="change_city")
-    builder.button(text="Изменить интересы", callback_data="change_category")
+    builder.button(text="▶︎ Создать свое событие", callback_data="create_post")
+    builder.button(text="❤︎ Мои события", callback_data="my_posts")
+    builder.button(text="⚙︎ Изменить город", callback_data="change_city")
+    builder.button(text="⚙︎ Изменить интересы", callback_data="change_category")
     builder.button(text="Помощь", callback_data="help")
     
     # Ключевое изменение - выравниваем все кнопки по левому краю
