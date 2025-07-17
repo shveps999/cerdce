112a2f3ebc9fa9d4606ff5134e75cbddbebded3b
shveps999 <privetzdarovahappy@gmail.com>
2025-07-17 19:25:05 +0300

    Update main_keyboard.py

--- events_bot/bot/keyboards/main_keyboard.py
+++ events_bot/bot/keyboards/main_keyboard.py
@@ -5,12 +5,12 @@ def get_main_keyboard() -> InlineKeyboardMarkup:
     """Главное меню с кнопкой избранного"""
     builder = InlineKeyboardBuilder()
     
-    builder.button(text="📮\u00A0Смотреть подборку", callback_data="feed")
-    builder.button(text="✏️\u00A0Создать событие", callback_data="create_post") 
-    builder.button(text="❤️\u00A0Моё избранное", callback_data="liked_posts")
-    builder.button(text="🎓\u00A0Изменить университет", callback_data="change_city")
-    builder.button(text="🌟\u00A0Изменить категории", callback_data="change_category")
-    builder.button(text="💬\u00A0Помощь", callback_data="help")
+    builder.button(text="📮 Смотреть подборку⠀⠀", callback_data="feed")
+    builder.button(text="✏️ Создать событие⠀⠀⠀⠀", callback_data="create_post") 
+    builder.button(text="❤️ Моё избранное⠀⠀⠀⠀⠀⠀", callback_data="liked_posts")
+    builder.button(text="🎓 Изменить университет", callback_data="change_city")
+    builder.button(text="🌟 Изменить категории⠀⠀", callback_data="change_category")
+    builder.button(text="💬 Помощь", callback_data="help")
     
     builder.adjust(1)
     return builder.as_markup()
